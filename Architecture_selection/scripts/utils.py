0658a23c4e894345df08a884579d0305c79884aa
Francesc0rtu <francescortu@live.it>
2022-10-25 18:04:37 +0200

    add denser encoder structure

--- Architecture_selection/scripts/utils.py
+++ Architecture_selection/scripts/utils.py
@@ -5,3 +5,11 @@ def compute_output_conv2d(input_shape, kernel_size, stride, padding, dilation):
             int((input_shape[1] + 2*padding[1] - dilation[1]*(kernel_size[1]-1) - 1)/stride[1] + 1))
     else:
         return int((input_shape + 2*padding - dilation*(kernel_size-1) - 1)/stride + 1)
+
+def compute_input_conv2d(output_shape, kernel_size, stride, padding, dilation):
+    "Compute the input shape of a 2D convolution layer."
+    if isinstance(output_shape, tuple) and len(output_shape)==2:
+        return (int((output_shape[0] - 1)*stride[0] + dilation[0]*(kernel_size[0]-1) - 2*padding[0] + 1),
+            int((output_shape[1] - 1)*stride[1] + dilation[1]*(kernel_size[1]-1) - 2*padding[1] + 1))
+    else:
+        return int((output_shape - 1)*stride + dilation*(kernel_size-1) - 2*padding + 1)
