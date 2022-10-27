1fb5006630290a6dfc31d509ef36f8aa1bade205
Francesc0rtu <francescortu@live.it>
2022-10-27 12:48:31 +0200

    now work on MINST and CIFAR, organize files

--- Architecture_selection/scripts/train.py
+++ Architecture_selection/scripts/train.py
@@ -1,78 +1,54 @@
+# train the network using SGD but without using all the training data at once
+from time import sleep
+from tqdm import tqdm
+import torch.nn as nn
 import torch
+import torch.optim as optim
 
-from .train_utils import accuracy, AverageMeter
-
-def train_epoch(model, dataloader, loss_fn, optimizer, loss_meter, performance_meter, performance, device):
-    for X, y in dataloader:
-        X = X.to(device)
-        y = y.to(device)
-        # 1. reset the gradients previously accumulated by the optimizer
-        #    this will avoid re-using gradients from previous loops
-        optimizer.zero_grad() 
-        # 2. get the predictions from the current state of the model
-        #    this is the forward pass
-        y_hat = model(X)
-        # 3. calculate the loss on the current mini-batch
-        loss = loss_fn(y_hat, y)
-        # 4. execute the backward pass given the current loss
-        loss.backward()
-        # 5. update the value of the params
-        optimizer.step()
-        # 6. calculate the accuracy for this mini-batch
-        acc = performance(y_hat, y)
-        # 7. update the loss and accuracy AverageMeter
-        loss_meter.update(val=loss.item(), n=X.shape[0])
-        performance_meter.update(val=acc, n=X.shape[0])
-
-
-def train_model(model, dataloader, loss_fn, optimizer, num_epochs, checkpoint_loc=None, checkpoint_name="checkpoint.pt", performance=accuracy, device="cuda"):
-
-    # create the folder for the checkpoints (if it's not None)
-    if checkpoint_loc is not None:
-        os.makedirs(checkpoint_loc, exist_ok=True)
+def train(model, trainloader, device=torch.device("cuda" if torch.cuda.is_available() else "cpu") ):
+    model.to(device)
+    inspected = 50000
+    iterations = int(inspected / 4)
     
-    model.train()
 
-    # epoch loop
-    for epoch in range(num_epochs):
-        loss_meter = AverageMeter()
-        performance_meter = AverageMeter()
+    dataloader_iterator = iter(trainloader)
 
-        train_epoch(model, dataloader, loss_fn, optimizer, loss_meter, performance_meter, performance, device)
+    # define the loss function and the optimizer
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
 
-        print(f"Epoch {epoch+1} completed. Loss - total: {loss_meter.sum} - average: {loss_meter.avg}; Performance: {performance_meter.avg}")
+    for i in tqdm(range(iterations)):
+        try:
+            inputs, labels = next(dataloader_iterator)
+            inputs, labels = inputs.to(device), labels.to(device)
+            # zero the parameter gradients
+            optimizer.zero_grad()
 
-        # produce checkpoint dictionary -- but only if the name and folder of the checkpoint are not None
-        if checkpoint_name is not None and checkpoint_loc is not None:
-            checkpoint_dict = {
-                "parameters": model.state_dict(),
-                "optimizer": optimizer.state_dict(),
-                "epoch": epoch
-            }
-            torch.save(checkpoint_dict, os.path.join(checkpoint_loc, checkpoint_name))
+            # calculate outputs by running images through the network
+            outputs = model(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
 
-    return loss_meter.sum, performance_meter.avg
-
-def test_model(model, dataloader, performance=accuracy, loss_fn=None, device="cuda"):
-    # create an AverageMeter for the loss if passed
-    if loss_fn is not None:
-        loss_meter = AverageMeter()
+        except StopIteration:
+            print("StopIteration, not enough data")
     
-    performance_meter = AverageMeter()
+    return model
+def eval(model, testloader, device=torch.device("cuda" if torch.cuda.is_available() else "cpu") ):
+  
+  correct = 0
+  total = 0
+  # since we're not training, we don't need to calculate the gradients for our outputs
+  with torch.no_grad():
+      for data in testloader:
+          images, labels = data
+          images, labels = images.to(device), labels.to(device)
+          # calculate outputs by running images through the network
+          outputs = model.forward(images)
+          # the class with the highest energy is what we choose as prediction
+          _, predicted = torch.max(outputs.data, 1)
+          total += labels.size(0)
+          correct += (predicted == labels).sum().item()
 
-    model.eval()
-    with torch.no_grad():
-        for X, y in dataloader:
-            X = X.to(device)
-            y = y.to(device)
-            y_hat = model(X)
-            loss = loss_fn(y_hat, y) if loss_fn is not None else None
-            acc = performance(y_hat, y)
-            if loss_fn is not None:
-                loss_meter.update(loss.item(), X.shape[0])
-            performance_meter.update(acc, X.shape[0])
-    # get final performances
-    fin_loss = loss_meter.sum if loss_fn is not None else None
-    fin_perf = performance_meter.avg
-    print(f"TESTING - loss {fin_loss if fin_loss is not None else '--'} - performance {fin_perf}")
-    return fin_loss, fin_perf
+  accuracy = 100 * correct // total
+  print(f'Accuracy of the network on the 10000 test images: {accuracy} %')
