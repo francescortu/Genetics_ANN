51cdbddd51533a0856236f7b45b8a7f7e713aba3
alexserra98 <alex.serra.pietro@gmail.com>
2022-11-11 18:45:38 +0100

    restored_main_test

--- DENSER/main_test.py
+++ DENSER/main_test.py
@@ -13,3 +13,11 @@ if __name__ == "__main__":
 
    print("TEST OF MUTATION AT GA LEVEL...")
    test_mutation_GA_level(trainloader)
+    
+   #print("TEST OF MUTATION AT DSGE LEVEL...")
+   #test_mutation_dsge_level(trainloader) 
+
+   #print("TEST EVOLUTION...")
+   #test_evolution(trainloader)
+
+
