27b38ab6d77383edb33706f6259e3d9f246192ba
erika.lena <lena.erika@gmail.com>
2022-11-17 14:45:19 +0100

    updated file for plotting results and added results on cifar

--- main.py
+++ main.py
@@ -120,7 +120,9 @@ if __name__ == "__main__":
     print(f"\n\n Evolution of a population of networks: \n dataset: {dataset}, population_size: {population_size}, number of generation: {num_generations},  batch size: {batch_size}, path: {subpath} \n\n")
     print("Running Device:", torch.device("cuda" if torch.cuda.is_available() else "cpu") )
     run_evolution(dataset, population_size, num_generations, batch_size, subpath = subpath) 
-    #read_results(population_size, subpath = subpath)
+    
+    #read_results(subpath = subpath)
+    #plot_net_representation(subpath = subpath)
 
     # check best network saved
     """ filename = f"results/{subpath}/best_organism.pkl"
